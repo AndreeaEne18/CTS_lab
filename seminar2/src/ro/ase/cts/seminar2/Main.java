11842f2033dbf4be31eb5579196c836b974ecf9c
AndreeaEne <eneandreea18@stud.ase.ro>
2021-03-11 08:20:34 +0200

    Done

--- seminar2/src/ro/ase/cts/seminar2/Main.java
+++ seminar2/src/ro/ase/cts/seminar2/Main.java
@@ -8,11 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		CurrentAccount c= new CurrentAccount (300, "IBAN1");
 		CurrentAccount account2 = new CurrentAccount(200,"IBAN2");
+		
+		SavingsAccount account3= new SavingsAccount(300, "IBAN3");
 		System.out.println("Suma disponibila este "+c.getBalance());
 		System.out.println("Creditul maxim pentru contul curent este " +CurrentAccount.MAX_CREDIT);
 		c.deposit(200);
 		System.out.println("Suma disponibila este "+c.getBalance());
-		//try pt ca am implementat metoda 
+		 
 		try {
 			c.withdraw(200);
 			c.transfer(100, c);
@@ -22,6 +24,11 @@ public class Main {
 		}
 		System.out.println("Suma disponibila este "+c.getBalance());
 		System.out.println("suma in contul 2 este: " + account2.getBalance() );
+		System.out.println("suma in contul  este: " + account3.getBalance() );
+		account3.addInterest(10);
+		System.out.println("suma in contul  este: " + account3.getBalance() );
+		Bank banca = new Bank();
+		BankAccount account4= banca.openBankAccount(AccountType.CURRENT);
 	}
 
 }
