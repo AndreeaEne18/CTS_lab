d8660e8627eb837cdf784351ab983c7027dc0463
Andreea <Andreea@DESKTOP-I0D6SQG>
2021-03-25 08:39:44 +0200

    Done!

---
+++ seminar_5/src/ro/ase/cts/seminar_5/refactory/Product.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.seminar_5.refactory;
+
+public interface Product {
+	public String getDescription();
+}
---
+++ seminar_5/src/ro/ase/cts/seminar_5/refactory/TechProduct.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.seminar_5.refactory;
+
+public class TechProduct implements Product{
+
+	@Override
+	public String getDescription() {
+		return "this is a tech product";
+	}
+
+}
